8b94749af849741dac94604e963bfd7c84bc70c5
Gameh Lazarus Hyaena <gemahursusonca@gmail.com>
2025-07-22 00:04:55 -0300

    Mock-up page with gallery

--- yearbook/script.js
+++ yearbook/script.js
@@ -4,57 +4,67 @@
  */
 
 const bgmList = [
-    "./audio/bgm1.ogg",
-    "./audio/bgm2.ogg"
+    './audio/bgm1.ogg',
+    './audio/bgm2.ogg'
 ];
 
 const characters = [
     {
-        name: "King",
-        superlative: "Most likely to chain a suplex after a hug",
-        portrait: "./img/char/port_king.webp",
-        button: "./img/char/sel_king.webp"
-    },
-    {
-        name: "Parasoul",
-        superlative: "Competitive fighting game star",
-        portrait: "./img/char/port_parasoul.webp",
-        button: "./img/char/sel_parasoul.webp"
-    },
-    {
-        name: "Josuke Higashikata",
-        superlative: "Feeling freshest after, like christmas morning",
-        portrait: "./img/char/port_josuke.png",
-        button: "./img/char/sel_josuke.png"
-    },
-    {
-        name: "Lae'zel",
-        superlative: "Put in detention for calling people \"Istik\"",
-        portrait: "./img/char/port_laezel.png",
-        button: "./img/char/sel_laezel.png"
+        id: 'hyaenaGemah',
+        name: 'Gemah Lazarus Hyaena',
+        superlative: '[Insert your superlative here]',
+        portraits: ['portrait.PNG', 'select.jpg'],
+        gallery: [
+            { src: './img/char/hyaenaGemah/gallery/202408.png', title: 'Aug 2024 - Pokemon Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202409.png', title: 'Sep 2024 - Mecha Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202410A.gif', title: 'Oct 2024 - Halloween Trade #1' },
+            { src: './img/char/hyaenaGemah/gallery/202410B.gif', title: 'Oct 2024 - Halloween Trade #1' },
+            { src: './img/char/hyaenaGemah/gallery/202411.png', title: 'Nov 2024 - Music Albums Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202412.png', title: 'Dec 2024 - Winter Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202501.png', title: 'Jan 2025 - Criminal Activity Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202502.png', title: 'Feb 2025 - Space Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202503.png', title: 'Mar 2025 - Beach Episode Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202504.png', title: 'Apr 2025 - High Fantasy Trade' },
+            { src: './img/char/hyaenaGemah/gallery/202506.png', title: 'Jun 2025 - Sports Trade' }
+        ]
     }
 ];
 
-const selectContainer = document.getElementById("selector");
+const selectContainer = document.getElementById('selector');
 
 characters.forEach(char => {
-    const btn = document.createElement("button");
-    btn.classList.add("charBtn");
-    btn.type = "button";
-    btn.innerHTML = `<img src="${char.button}" style="border: none; border-radius: 8px;" height="64" width="64" />`;
+    const btn = document.createElement('button');
+    btn.classList.add('charBtn');
+    btn.type = 'button';
+    btn.innerHTML = `<img src="./img/char/${char.id}/${char.portraits[1]}" style="border: none; border-radius: 8px;" height="64" width="64" />`;
     btn.onmouseenter = () => {
-        const sfxHov = document.getElementById("sfxHover");
+        const sfxHov = document.getElementById('sfxHover');
         sfxHov.pause();
         sfxHov.currentTime = 0;
         sfxHov.play();
     };
     btn.onclick = () => {
-        document.getElementById("charName").innerHTML = char.name;
-        document.getElementById("charSuperlative").innerHTML = char.superlative;
-        const port = document.getElementById("charPortrait");
-        port.src = char.portrait;
+        // Portrait section behavior
+        document.getElementById('charName').innerHTML = char.name;
+        document.getElementById('charSuperlative').innerHTML = char.superlative;
+        const port = document.getElementById('charPortrait');
+        port.src = `./img/char/${char.id}/${char.portraits[0]}`;
         port.alt = char.name;
-        const sfxSel = document.getElementById("sfxClick");
+
+        // Gallery behavior
+        const gallery = document.getElementById('charGallery');
+        gallery.innerHTML = '';
+        
+        for (let i = 0; i < char.gallery.length; i++) {
+            gallery.innerHTML += `<a class="a-picture" href="${char.gallery[i].src}" title="${char.gallery[i].title}">
+                <img src="${char.gallery[i].src}" alt="${char.gallery[i].title}" />
+            </a>`;
+        }
+
+        new SimpleLightbox({elements: '#charGallery a'});
+
+        // Sound behavior
+        const sfxSel = document.getElementById('sfxClick');
         sfxSel.pause();
         sfxSel.currentTime = 0;
         sfxSel.play();
@@ -62,9 +72,9 @@ characters.forEach(char => {
     selectContainer.append(btn);
 });
 
-const bgmPlayer = document.getElementById("bgmusic");
+const bgmPlayer = document.getElementById('bgmusic');
 bgmPlayer.volume = 0.2;
-bgmPlayer.addEventListener("ended", () => {
+bgmPlayer.addEventListener('ended', () => {
     const nextSong = bgmList.pop();
     bgmPlayer.pause();
     bgmPlayer.src = nextSong;
